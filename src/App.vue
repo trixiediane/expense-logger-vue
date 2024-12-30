254ecc7605aefee083acd1bfdf228a6bf7099453
trixiediane <swiftsklossy490@gmail.com>
2024-12-30 14:42:32 +0800

    Creating and importing of components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
-
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
 </script>
 
 <template>
-  My App
+  <Header />
+
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
 </template>
